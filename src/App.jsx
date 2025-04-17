de201d54f63c1a597c77302a674f474592b67984
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-17 18:15:47 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 
 import NavBar from './components/NavBar'
 import SideBar from './components/SideBar'
+import { Routes,Route } from 'react-router-dom'
+import Add from './pages/Add'
+import Order from './pages/Order'
+import List from './pages/List'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +18,13 @@ function App() {
     <hr />
     <div className='flex w-full'>
        <SideBar/>
+       <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+           <Routes>
+            <Route path="/add" element={<Add/>}/>
+            <Route path='/list' element={<List/>}/>
+            <Route path='/orders' element={<Order/>}/>
+           </Routes>
+       </div>
     </div>
     </>
 
