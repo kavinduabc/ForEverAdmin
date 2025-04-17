2a0f196af38878cf95514d808f6e80571c2b3bd6
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-17 11:57:28 +0530

    fixd some error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Admin from './admin/Admin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <p>hgh</p>
+    <Admin/>
     </>
   )
 }
---
+++ src/admin/Admin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='bg-red-600'>Admin</div>
+  )
+}
+
+export default Admin
