ed078f030c152fe9de95b067acad07ed195c8841
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-17 15:49:20 +0530

    some import library s

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -4,7 +4,9 @@ const Admin = () => {
   return (
     <div className='w-full hiscreen flex'>
         <div className='w-[200px] h-full bg-blue-800 '>
-            
+            <button className='w-full h-[40px] text-[27px] font-bold flex justify-center items-center '>
+                
+            </button>
         </div>
 
     </div>
