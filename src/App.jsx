ef62bbe9e56fc7b738e0ba1a5bd398a88a4df3b3
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-18 08:29:21 +0530

    done authentication

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,31 @@ import Order from './pages/Order'
 import List from './pages/List'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [token,setToken] = useState("")
+
+
 
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <>
-    <NavBar/>
-    <hr />
-    <div className='flex w-full'>
-       <SideBar/>
-       <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
-           <Routes>
-            <Route path="/add" element={<Add/>}/>
-            <Route path='/list' element={<List/>}/>
-            <Route path='/orders' element={<Order/>}/>
-           </Routes>
-       </div>
-    </div>
-    </>
+      {
+        token === "" ? <Login/> :
+        <>
+        <NavBar/>
+        <hr />
+        <div className='flex w-full'>
+           <SideBar/>
+           <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+               <Routes>
+                <Route path="/add" element={<Add/>}/>
+                <Route path='/list' element={<List/>}/>
+                <Route path='/orders' element={<Order/>}/>
+               </Routes>
+           </div>
+        </div>
+        </>
+      }
+    
 
     </div>
   )
