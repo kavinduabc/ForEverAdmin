6752e1f3242a61f7181677994d62936414c9c634
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-21 23:27:47 +0530

    half complete

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { backendUrl } from '../App';
+import axios from 'axios';
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const [product, setProduct] = useState([]);
+  const [productLoaded, setProductLoaded] = useState(false);
+  const navigate = useNavigate();
 
-export default List
+  useEffect(() => {
+    if (!productLoaded) {
+      const token = localStorage.getItem('token');
+
+      axios.get(`${backendUrl}/api/product`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setProduct(res.data);
+        setProductLoaded(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  }, [productLoaded]);
+
+  return (
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {product.length > 0 ? (
+              product.map((product) => (
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.email}</td>
+                  <td>{product.role}</td>
+                  <td>Action</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="5">No Product Available</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default List;
